32497305cd61afcc594e14ab5725667dd977cbcd
stelsol <132797421+stelsol@users.noreply.github.com>
2026-06-14 00:08:24 -0300

    Ex com react

--- projeto_1/src/App.jsx
+++ projeto_1/src/App.jsx
@@ -1,13 +1,41 @@
 import "./App.css";
+import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
-  const nome = "Stella";
-  const estilo = { color: "blue", fontSize: "18px" };
+  const [valor, setValor] = useState("");
+  const [resultado, setResultado] = useState("");
+
+  function Gerar() {
+    let num = Number(valor);
+    let contador = " ";
+
+    for (let i = 0; i <= num; i++) {
+      // console.log(contador);
+      contador += i + " ";
+    }
+    setResultado(contador);
+  }
+
   return (
-    <div className="app">
-      <h2 style={estilo}>{nome.toUpperCase()}</h2>
-      <p>Bem-vindo ao React!</p>
-    </div>
+    <main className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+      <div className="bg-purple-600">
+        <input
+          type="text"
+          name="valor"
+          id="valor"
+          placeholder="Digite um numero"
+          value={valor}
+          onChange={(e) => setValor(e.target.value)}
+        />
+        <button id="botao" className="bg-purple-950" onClick={Gerar}>
+          Gerar
+        </button>
+        <div id="resultado" className="bg-purple-100">
+          {resultado}
+        </div>
+      </div>
+    </main>
   );
 }
 
